29285c817d919f63eb4ffd52fdeeed0f6dfe0053
faiqamikom <101610607+faiqamikom@users.noreply.github.com>
2022-04-19 17:25:28 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,22 +6,25 @@ using System.Threading.Tasks;
 
 namespace ResponsiPemrograman4274
 {
-    class Karyawan
+    class Program
     {
         static void Main(string[] args)
         {
-            Karyawan data1 = new Karyawan(190302123, "Paijo", 3000000);
+            Karyawan data1 = new Karyawan(190302123, "Paijo", 3000000);  
             Karyawan data2 = new Karyawan(190302124, "Jono", 2000000);
 
-            Console.WriteLine("No Nik/Nama       Gaji Bulanan");
-            Console.WriteLine("------------------------------");
+            Console.WriteLine("No Nik/Nama            Gaji Bulanan");
+            Console.WriteLine("-----------------------------------");
             Console.WriteLine("1. " + data1.nik + " " + data1.nama + "     " + data1.gajibulanan);
-            Console.WriteLine("2. " + data2.nik + " " + data2.nama + "     " + data2.gajibulanan);
+            Console.WriteLine("2. " + data2.nik + " " + data2.nama + "      " + data2.gajibulanan);
             Console.WriteLine();
-            Console.WriteLine("No Nik/Nama       Gaji Bulanan");
-            Console.WriteLine("------------------------------");
-            Console.WriteLine("1. " + data1.nik + " " + data1.nama + "     " + data1.gajibulanan * 110 / 100);
-            Console.WriteLine("2. " + data2.nik + " " + data2.nama + "     " + data2.gajibulanan * 110 / 100);
+            Console.WriteLine();
+            Console.WriteLine("Asyiiiiik kenaikan gaji 10%!!");
+            Console.WriteLine();
+            Console.WriteLine("No Nik/Nama            Gaji Bulanan");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("1. " + data1.nik + " " + data1.nama + "     " + data1.gajibulanan*110/100);
+            Console.WriteLine("2. " + data2.nik + " " + data2.nama + "      " + data2.gajibulanan*110/100);
 
             Console.ReadKey();
         }
